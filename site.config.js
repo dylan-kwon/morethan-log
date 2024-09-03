981efaef3cf791d4b3fc9d5424e0b1d0c4f116b6
dylan-kwon <dylan.kwon98@gmail.com>
2024-09-03 21:51:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -54,7 +54,7 @@ const CONFIG = {
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
