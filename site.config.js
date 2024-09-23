b06438531ad682ab46efe50e37ce2c504c6c8dc8
dylan-kwon <dylan.kwon98@gmail.com>
2024-09-23 20:56:56 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -30,13 +30,13 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "Dylan Blog",
+    title: "Dylan Dev",
     description: "Welcome to my blog",
     scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://dylan-kwon.vercel.app",
+  link: "https://dylan-dev.vercel.app",
   since: "", // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
