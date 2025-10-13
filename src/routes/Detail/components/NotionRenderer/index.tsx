18381b670d2d575889145451481d226e0b358a10
dylan-kwon <dylan.kwon98@gmail.com>
2025-10-13 13:57:03 +0900

    Update index.tsx

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -22,78 +22,7 @@ const _NotionRenderer = dynamic(
 )
 
 const Code = dynamic(() =>
-  import('react-notion-x/build/third-party/code').then(async (m) => {
-    // additional prism syntaxes
-    await Promise.all([
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-markup-templating.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-markup.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-bash.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-c.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-cpp.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-csharp.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-docker.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-java.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-js-templates.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-coffeescript.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-diff.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-git.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-go.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-graphql.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-handlebars.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-less.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-makefile.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-markdown.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-objectivec.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-ocaml.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-python.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-reason.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-rust.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-sass.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-scss.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-solidity.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-sql.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-stylus.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-swift.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-wasm.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-yaml.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-kotlin.js'),
-      // @ts-expect-error ignore no prisma types
-      import('prismjs/components/prism-dart.js')
-    ])
-    return m.Code
-  })
+  import("react-notion-x/build/third-party/code").then(async (m) => m.Code)
 )
 
 const Collection = dynamic(() =>
